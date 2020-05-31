882c2afcad740d80292bc89cb79701edf5f8088c
Ryosuke <ryo.m.code@gmail.com>
2020-06-01 02:23:46 +0900

    Add optimisticResponse to LikeLyric mutation

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,10 +3,18 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class LyricList extends Component {
-    onLike(id) {
+    onLike(id, likes) {
         this.props.mutate({
-            variables: {id: id}
-        })
+            variables: { id },
+            optimisticResponse: {
+                __typename: 'Mutation',
+                likeLyric: {
+                    id,
+                    __typename: 'LyricType',
+                    likes: likes + 1
+                }
+            }
+        });
     }
 
     renderLyrics() {
@@ -17,7 +25,7 @@ class LyricList extends Component {
                     <div className="vote-box">
                         <i
                             className="material-icons"
-                            onClick={() => this.onLike(id)}
+                            onClick={() => this.onLike(id, likes)}
                         >
                             thumb_up
                         </i>
